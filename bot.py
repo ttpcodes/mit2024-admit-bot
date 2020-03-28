fc32920c89a19afefd28a254d5ba63a8b19120b4
Emma <emmabat@mit.edu>
2020-03-28 19:15:10 -0400

    added database logic

--- bot.py
+++ bot.py
@@ -1,3 +1,6 @@
+import mysql.connector
+from mysql.connector import errorcode
+
 from discord import Embed
 from discord.ext.commands import Bot, command, CommandError
 
@@ -8,6 +11,10 @@ from smtplib import SMTP_SSL
 from typing import Optional
 
 DEFAULT_ROLE_USER_DATA = []
+USER = "user"
+PASS = "password"
+HOST = "sql.mit.edu"
+DB_NAME = "emmabat+mit2024"
 
 
 def generate_embed_template(ctx, title, error=False):
@@ -29,47 +36,83 @@ class AdmitBot(Bot):
 
     @command(brief='Verify your Discord account for a Guild.')
     async def verify(self, ctx, email: str, token: Optional[str]):
-        if not token:
-            try:
-                # Need to obtain discriminator from database for specified email
-                if discriminator:
-                    raise CommandError('Email `{}` is already associated with a Discord account'.format(email))
-                else:
-                    gen = token_hex(32)
-                    expiry = datetime.now() + timedelta(hours=24)
-                    # Set token = gen and expiry = expiry for specified email.
-                    smtp = SMTP_SSL('outgoing.mit.edu')
-                    smtp.login('', '') # TODO: config file
-                    msg = MIMEText('Hello!<br><br>\n' +
-                                   'To verify your email address, please send the following command to the bot:<br>\n' +
-                                   '<pre>!verify {} {}</pre><br><br>\n'.format(email, gen), 'html')
-                    msg['Subject'] = 'Discord - MyMIT Verification'
-                    msg['From'] = 'Discord - MyMIT Bot <mit2024bot@mit.edu>'
-                    msg['To'] = email
-                    smtp.sendmail('mit2024bot@mit.edu', email, msg.as_string())
-                    embed = generate_embed_template(ctx, 'Verification Requested Successfully')
-                    embed.description = 'Please check your inbox at `{}` for further instructions'.format(email)
-                    await ctx.send(embed=embed)
-                    return
-            except ProgrammingError:
-                raise CommandError('Email `{}` was not recognized as a valid MyMIT email'.format(email))
-        # Logic here has a token specified, so we attempt verification.
-        # Get discriminator, token, and expiry for specified email
         try:
-            if discriminator:
-                raise CommandError('Email `{}` is already associated with a Discord account'.format(email))
-            elif expiry < datetime.now():
-                raise CommandError('Token has expired! Run `!verify {}` for a new token.'.format(email))
-            elif stored != token:
-                raise CommandError("Token is incorrect! Double check and make sure it is correct.")
+            connection = mysql.connector.connect(
+                user=USER, 
+                password = PASS,
+                host = HOST,
+                database=DB_NAME)
+            cursor = connection.cursor()
+            query = ("SELECT EXISTS(SELECT * FROM `{}` WHERE email = \'{}\')".format("users", email))
+            cursor.execute(query)
+            email_exists = cursor.fetchone()[0]
+            if email_exists:
+                # Need to obtain discriminator from database for specified email
+                query = ("SELECT discriminator FROM `{}` WHERE email = '{}'".format("users", email))
+                cursor.execute(query)
+                discriminator = cursor.fetchone()[0]
+                if not token:
+                    if discriminator:
+                        raise CommandError('Email `{}` is already associated with a Discord account'.format(email))
+                    else:
+                        gen = token_hex(32)
+                        expiry = datetime.now() + timedelta(hours=24)
+                        # TODO put gen, expiry into database
+                        query = ("UPDATE users SET token = '{}', token_expiry = '{}' "
+                                "WHERE email = '{}'".format(gen, expiry, email))
+                        cursor.execute(query)
+                        connection.commit()
+                        # Set token = gen and expiry = expiry for specified email.
+                        smtp = SMTP_SSL('outgoing.mit.edu')
+                        smtp.login('', '') # TODO: config file
+                        msg = MIMEText('Hello!<br><br>\n' +
+                                    'To verify your email address, please send the following command to the bot:<br>\n' +
+                                    '<pre>!verify {} {}</pre><br><br>\n'.format(email, gen), 'html')
+                        msg['Subject'] = 'Discord - MyMIT Verification'
+                        msg['From'] = 'Discord - MyMIT Bot <mit2024bot@mit.edu>'
+                        msg['To'] = email
+                        smtp.sendmail('mit2024bot@mit.edu', email, msg.as_string())
+                        embed = generate_embed_template(ctx, 'Verification Requested Successfully')
+                        embed.description = 'Please check your inbox at `{}` for further instructions'.format(email)
+                        await ctx.send(embed=embed)
+                        return
+                # Logic here has a token specified, so we attempt verification.
+                # Get discriminator, token, and expiry for specified email
+                else:
+                    if discriminator:
+                        raise CommandError('Email `{}` is already associated with a Discord account'.format(email))
+                    
+                    query = ("SELECT token, token_expiry FROM users WHERE email = '{}'".format(email))
+                    cursor.execute(query)
+                    expiry, stored = cursor.fetchone()
+
+                    if expiry < datetime.now():
+                        raise CommandError('Token has expired! Run `!verify {}` for a new token.'.format(email))
+                    elif stored != token:
+                        raise CommandError("Token is incorrect! Double check and make sure it is correct.")
+                    else:
+                        # Verified. For the specified email, set username and discriminator. Token and expiry should just be
+                        # None.
+                        username = ctx.message.author.name
+                        discriminator = ctx.message.author.discriminator
+                        query = ("UPDATE users SET username = '{}', discriminator = '{}', "
+                                "token = NULL, token_expiry = NULL, "
+                                "WHERE email = '{}'".format(username, discriminator, email))
+                        cursor.execute(query)
+                        connection.commit()
+                        # TODO put username, discriminator into database; set token, expiry to NULL
+                        embed = generate_embed_template(ctx, 'Account Verified Successfully')
+                        embed.description = "Contact the admins if you still can't access the server."
+                        await ctx.send(embed=embed)
+                        return
             else:
-                # Verified. For the specified email, set username and discriminator. Token and expiry should just be
-                # None.
-                username = ctx.message.author.name
-                discriminator = ctx.message.author.discriminator
-                embed = generate_embed_template(ctx, 'Account Verified Successfully')
-                embed.description = "Contact the admins if you still can't access the server."
-                await ctx.send(embed=embed)
-                return
-        except ProgrammingError:
-            raise CommandError('Email `{}` was not recognized as a valid MyMIT email'.format(email))
+                raise CommandError('Email `{}` was not recognized as a valid MyMIT email'.format(email))
+        except mysql.connector.Error as err:
+            if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+                raise CommandError("Check your username or password.")
+            elif err.errno == errorcode.ER_BAD_DB_ERROR:
+                raise CommandError("Database does not exist.")
+            else:
+                raise CommandError(err)
+        else:
+            connection.close()
